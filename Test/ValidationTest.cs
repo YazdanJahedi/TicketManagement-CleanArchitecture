c561153928f6e25c1e0f1592096d8ba2b10bedd9
Yazdan <yzdn.jt@gmail.com>
2023-07-16 16:27:59 +0330

    Add Test folder with a few simple tests for validators

---
+++ Test/ValidationTest.cs
@@ -0,0 +1,79 @@
+using Application.Features.CreateUser;
+using Application.Features.LoginUser;
+
+namespace Test
+{
+    [TestClass]
+    public class ValidationTest
+    {
+        [TestMethod]
+        public void TestCreateUserRequestValidation1()
+        {
+            CreateUserRequest req = new CreateUserRequest
+            { 
+                Email = "ali.123",   // email is not valid
+                Name  = "ali",
+                Password = "something",
+                PhoneNumber = "1234567890",              
+            };
+
+            CreateUserValidator validator = new();
+            var validatorResult = validator.Validate(req);
+
+            if (validatorResult.IsValid)
+                Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestCreateUserRequestValidation2()
+        {
+            CreateUserRequest req = new CreateUserRequest
+            {
+                Email = "ali@ali.com",   // email is valid
+                Name = "ali",
+                Password = "something",
+                PhoneNumber = "1234567890",
+            };
+
+            CreateUserValidator validator = new();
+            var validatorResult = validator.Validate(req);
+
+            if (!validatorResult.IsValid)
+                Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestLoginRequestValidation1()
+        {
+            LoginRequest req = new LoginRequest
+            {
+                Email = "hassan@",   // email is not valid
+                Password = "something",
+            };
+
+            LoginRequestValidator validator = new();
+            var validatorResult = validator.Validate(req);
+
+            if (validatorResult.IsValid)
+                Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestLoginRequestValidation2()
+        {
+            LoginRequest req = new LoginRequest
+            {
+                Email = "ali33@ali12.com",   // email is valid
+                Password = "something",
+            };
+
+            LoginRequestValidator validator = new();
+            var validatorResult = validator.Validate(req);
+
+            if (!validatorResult.IsValid)
+                Assert.Fail();
+        }
+    }
+}
+
+
