00da84a6026064bd6332484bab47fa2353c3c419
Yazdan <yzdn.jt@gmail.com>
2023-08-15 11:21:59 +0330

    Cleanup Domain Entities

--- Domain/Common/BaseEntity.cs
+++ Domain/Common/BaseEntity.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-namespace Domain.Common
+﻿namespace Domain.Common
 {
     public record BaseEntity
     {
--- Domain/Entities/Ticket.cs
+++ Domain/Entities/Ticket.cs
@@ -1,6 +1,5 @@
 ﻿using Domain.Common;
 using Domain.Enums;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
