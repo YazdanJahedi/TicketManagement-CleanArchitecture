82d8e2d164bb58c398f3110ccf315dd8c4832d9c
Yazdan <yzdn.jt@gmail.com>
2023-08-20 15:12:21 +0330

    Add UnitOfSerive

---
+++ Application/Interfaces/Service/IUnitOfSevice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Service
+{
+    public interface IUnitOfSevice
+    {
+        IAuthService AuthService { get; }
+        IFaqService FaqService { get; }
+        IMessageAttachmentService MessageAttachmentService { get; }
+        IMessageService MessageService { get; }
+        ITicketService TicketService { get; }
+        
+    }
+}
---
+++ Infrastructure/Services/UnitOfSevice.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces.Service;
+
+namespace Infrastructure.Services
+{
+    public class UnitOfSevice : IUnitOfSevice
+    {
+        public UnitOfSevice(IAuthService authService, IFaqService faqService, IMessageAttachmentService messageAttachmentService,
+                            IMessageService messageService, ITicketService ticketService)
+        { 
+            AuthService = authService;
+            FaqService = faqService;
+            MessageAttachmentService = messageAttachmentService;
+            TicketService = ticketService;
+            MessageService = messageService;
+        }
+        public IAuthService AuthService { get; set; }
+
+        public IFaqService FaqService { get; set; }
+
+        public IMessageAttachmentService MessageAttachmentService { get; set; } 
+
+        public IMessageService MessageService { get; set; }
+
+        public ITicketService TicketService { get;set; }
+    }
+}
